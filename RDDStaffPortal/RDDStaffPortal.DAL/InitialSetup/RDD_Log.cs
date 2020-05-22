9835a7ff63f6301e6f9dfade131da8998fec91a4
Pramod Dongre <pramod@reddotdistribution.com>
2020-05-22 18:52:41 +0300

    Pramod - added Common class to write all common functions

---
+++ RDDStaffPortal/RDDStaffPortal.DAL/InitialSetup/RDD_Log.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDDStaffPortal.DAL.InitialSetup
+{
+    class RDD_Log
+    {
+       public int Id { get; set;}
+       public string TableName { get; set; }
+       public int KeyId { get; set; }
+       public string ColName { get; set; }
+       public string ColDescription { get; set; }
+       public string OldValue { get; set; }
+       public string NewValue { get; set; }
+       public string ChangedBy { get; set; }
+       public DateTime ChangedOn { get; set; }
+    }
+}
