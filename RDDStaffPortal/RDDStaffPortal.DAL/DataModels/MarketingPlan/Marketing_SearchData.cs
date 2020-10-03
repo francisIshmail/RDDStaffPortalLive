23829d7adcd0511e8fe0bbcca2409c2e28c5b180
Trupti D Suryawanshi <trptsuryawanshi@outlook.com>
2020-10-03 16:02:21 +0530

    Trupti - Marketing Plan

---
+++ RDDStaffPortal/RDDStaffPortal.DAL/DataModels/MarketingPlan/Marketing_SearchData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDDStaffPortal.DAL.DataModels.MarketingPlan
+{
+   public  class Marketing_SearchData
+    {
+        public int Planid { get; set; }
+       public string SourceOfFund { get; set; }
+         public string CountryName{ get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Vendor { get; set; }
+        public string VendorApprovedAmt { get; set; }
+        public string RDDApprovedAmt { get; set; }
+        public string UsedAmount { get; set; }
+        public string BalanceAmount { get; set; }
+        public string BalanceFromApp { get; set; }
+        public string Description { get; set; }
+        public string  ApprovedBy { get; set; }
+        public string planStatus { get; set; }
+        public string ApprovalStatus { get; set; }
+        public string CreatedOn { get; set; }
+    }
+}
---
+++ RDDStaffPortal/RDDStaffPortal.DAL/MarketingPlan/MarketingPlanLines.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDDStaffPortal.DAL.MarketingPlan
+{
+   public class MarketingPlanLines
+    {
+        public string PlanId { get; set; }
+public string LineRefNo { get; set; }
+        public string VenderPONo { get; set; }
+        public string SAPPONo { get; set; }
+        public string ActivityDate { get; set; }
+        public string Description { get; set; }
+        public string Vendor { get; set; }
+        public string Amount { get; set; }
+        public string Country { get; set; }
+        public string CountryName { get; set; }
+        public string Status { get; set; }
+        public string Status1 { get; set; }
+        public string ApprovedBy { get; set; }
+        public string ApprovedOn { get; set; }
+        public string ApproverRemark { get; set; }
+        public string CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastUpdatedOn { get; set; }
+        public string Lastupdatedby { get; set; }
+        public string Flag { get; set; }
+    }
+}
