6396033ac5a9227de0d53fde12ff5de21f52e190
Pramod D <psdongre313@gmail.com>
2020-12-08 13:51:07 +0530

    Revert "Added comments in Account Controller"
    This reverts commit 854a820ecab80621cba1345ecca0541d5a2cd0e8.

--- RDDStaffPortal/RDDStaffPortal/Controllers/AccountController.cs
+++ RDDStaffPortal/RDDStaffPortal/Controllers/AccountController.cs
@@ -24,7 +24,6 @@ namespace RDDStaffPortal.Controllers
             return View();
         }
         
-        // Login view
         public ActionResult Login()
         {
             return View();
