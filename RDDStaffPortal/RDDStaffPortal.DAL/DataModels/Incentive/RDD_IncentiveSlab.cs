f575d4660c22d86b038a99da160185585aeb0af4
Pratim Dhar <psdongre313@outlook.com>
2020-12-09 12:32:56 +0530

    Added Comp & Incentive module

---
+++ RDDStaffPortal/RDDStaffPortal.DAL/DataModels/Incentive/RDD_IncentiveSlab.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+namespace RDDStaffPortal.DAL.DataModels.Incentive
+{
+    public class RDD_IncentiveSlab
+    {
+        public int SlabId { get; set; }
+        public string Years { get; set; }
+        public string Period { get; set; }
+        public string IsDeleted { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedOn { get; set; }
+        public List<SelectListItem> DesignationNameList { get; set; }
+        public List<SelectListItem> YearList { get; set; }
+        public bool EditFlag { get; set; }
+        public bool SaveFlag { get; set; }
+        public string ErrorMsg { get; set; }
+        public int id { get; set; }
+        public int pagesize { get; set; }
+        public int pageno { get; set; }
+        public string psearch { get; set; }
+        public int TotalCount { get; set; }
+        public int RowNum { get; set; }
+        public string RType { get; set; }
+        public string Ptype { get; set; }
+        public List<RDD_IncentiveSlabs_Trans> RDD_IncentiveSlabs_TransList { get; set; }
+    }
+    public partial class RDD_IncentiveSlabs_Trans
+    {
+        public int TransSlabId { get; set; }
+        public int SlabId { get; set; }
+        public string Achieved_Percentage_From { get; set; }
+        public string Achieved_Percentage_To { get; set; }
+        public string Incentive_Percentage { get; set; }
+        public string IsDeleted { get; set; }
+    }
+}
---
+++ RDDStaffPortal/RDDStaffPortal/Areas/Incentive/IncentiveAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace RDDStaffPortal.Areas.Incentive
+{
+    public class IncentiveAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Incentive";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Incentive_default",
+                "Incentive/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
