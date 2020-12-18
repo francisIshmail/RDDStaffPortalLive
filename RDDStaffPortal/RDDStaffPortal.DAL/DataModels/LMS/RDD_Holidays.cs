b6192b1a37accea7e3a118afa7ba0791ba0c4912
Avijeet Tiwari <avijeet@reddotdistribution.com>
2020-12-18 21:19:47 +0530

    Added Holiday Master

--- RDDStaffPortal/RDDStaffPortal.DAL/DataModels/LMS/RDD_Holidays.cs
+++ RDDStaffPortal/RDDStaffPortal.DAL/DataModels/LMS/RDD_Holidays.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using System.Windows.Documents;
 
 namespace RDDStaffPortal.DAL.DataModels.LMS
 {
     public partial class RDD_Holidays
     {
+        public List<SelectListItem> CountryList { get; set; }
         public int HolidayId { get; set; }
-        public int CountryCode { get; set; }
+        public string CountryCode { get; set; }
         public string HolidayName { get; set; }
         public DateTime? HolidayDate { get; set; }
         public bool IsDeleted { get; set; }
@@ -18,6 +20,7 @@ namespace RDDStaffPortal.DAL.DataModels.LMS
         public DateTime? CreatedOn { get; set; }
         public string LastUpdatedBy { get; set; }
         public DateTime? LastUpdatedOn { get; set; }
+        public bool HR { get; set; }
 
         public bool DeleteFlag { get; set; }
 
@@ -25,8 +28,16 @@ namespace RDDStaffPortal.DAL.DataModels.LMS
 
         public bool Editflag { get; set; }
 
+        public bool UpdateFlag { get; set; }
+
         public string ActionType { get; set; }
+        public int id { get; set; }
 
         public string ErrorMsg { get; set; }
+    }
+    public partial class GetCountryDetails
+    {
+        public string CountryCode { get; set; }
+        public string Country { get; set; }
     }
 }
