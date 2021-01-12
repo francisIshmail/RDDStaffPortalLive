b61f05f83d6b9d8bd6f9b4acc608d09311146428
Mainak Ghosh <mainakg81@outlook.com>
2021-01-12 11:22:13 +0530

    LMS Leave request and leave request approval and dashboard

--- RDDStaffPortal/RDDStaffPortal/Areas/LMS/Controllers/RDD_LeaveRequestController.cs
+++ RDDStaffPortal/RDDStaffPortal/Areas/LMS/Controllers/RDD_LeaveRequestController.cs
@@ -6,17 +6,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using RDDStaffPortal.DAL;
+using System.Net.Mail;
+using Newtonsoft.Json;
 
 namespace RDDStaffPortal.Areas.LMS.Controllers
-{
+{   
     [Authorize]
     public class RDD_LeaveRequestController : Controller
     {
+        CommonFunction Com = new CommonFunction();
         RDD_LeaveRequest_Db_Operation rDD_LeaveRequest_TemplatesDb = new RDD_LeaveRequest_Db_Operation();
         // GET: LMS/RDD_LeaveRequest
         public ActionResult Index()
         {
-            return View();
+            RDD_LeaveRequest rDD_LeaveRequest = new RDD_LeaveRequest();
+            rDD_LeaveRequest.EmployeeLists = rDD_LeaveRequest_TemplatesDb.GetEmployee();                       
+            rDD_LeaveRequest.EmployeeId = rDD_LeaveRequest_TemplatesDb.GetEmployeeIdByLoginName(User.Identity.Name);
+            rDD_LeaveRequest.LeaveTypeList = rDD_LeaveRequest_TemplatesDb.GetLeaveTypeName(rDD_LeaveRequest.EmployeeId);
+            rDD_LeaveRequest.WeeklyOffDays = rDD_LeaveRequest_TemplatesDb.GetWeeklyOffDay(rDD_LeaveRequest.EmployeeId);
+            DataSet ds = rDD_LeaveRequest_TemplatesDb.GetHRRole(User.Identity.Name);
+            if (ds.Tables.Count > 0)
+            {
+
+
+                rDD_LeaveRequest.HR = Convert.ToBoolean(ds.Tables[0].Rows[0]["IsHRUser"].ToString());
+
+
+            }
+            return View(rDD_LeaveRequest);
         }
         public ActionResult GetLeaveRequest_List()
         {
@@ -26,21 +47,168 @@ namespace RDDStaffPortal.Areas.LMS.Controllers
             return PartialView(modules);
         }
         [Route("SaveLeaveRequest")]
-        public ActionResult SaveHolidays(RDD_LeaveRequest rDD_LeaveRequest)
+        public ActionResult SaveLeaveRequest(RDD_LeaveRequest RDD_LeaveRequest)
         {
-            rDD_LeaveRequest.CreatedBy = User.Identity.Name;
-            rDD_LeaveRequest.CreatedOn = DateTime.Now;
-            if (rDD_LeaveRequest.Editflag == true)
+            RDD_LeaveRequest.CreatedBy = User.Identity.Name;
+            RDD_LeaveRequest.CreatedOn = DateTime.Now;
+            if (RDD_LeaveRequest.Editflag == true)
             {
-                rDD_LeaveRequest.LastUpdatedBy = User.Identity.Name;
-                rDD_LeaveRequest.LastUpdatedOn = System.DateTime.Now;
+                RDD_LeaveRequest.LastUpdatedBy = User.Identity.Name;
+                RDD_LeaveRequest.LastUpdatedOn = System.DateTime.Now;
             }
-            return Json(new { data = rDD_LeaveRequest_TemplatesDb.Save(rDD_LeaveRequest) }, JsonRequestBehavior.AllowGet);
+            return Json( rDD_LeaveRequest_TemplatesDb.Save(RDD_LeaveRequest), JsonRequestBehavior.AllowGet);
         }
         [Route("GetLeaveRequest")]
         public ActionResult GetData(string LeaveRequestId)
         {
             return Json(new { data = rDD_LeaveRequest_TemplatesDb.GetData(LeaveRequestId) }, JsonRequestBehavior.AllowGet);
+        }
+        [Route("GetWeeklyOffDay")]
+        public ActionResult GetWeeklyOff(int EmployeeId)
+        {
+            return Json(new { data = rDD_LeaveRequest_TemplatesDb.GetWeeklyOffDay(EmployeeId) }, JsonRequestBehavior.AllowGet);
+        }
+       
+        [HttpPost]
+        public JsonResult UploadDoc(string EmployeeId)
+        {
+            string fname = "";
+            string strMappath = "";
+            string _imgname = string.Empty;
+            string _imgname1 = string.Empty;
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    string str = EmployeeId + "_" + User.Identity.Name;
+                    //strMappath = "~/excelFileUpload/" + "PV/" + User.Identity.Name + "/" + type + "/";
+                    strMappath = "~/LMSFileUpload/" + "LRQ/" + str + "/";
+
+                    if (!Directory.Exists(strMappath))
+                    {
+                        Directory.CreateDirectory(System.IO.Path.Combine(Server.MapPath(strMappath)));
+                    }
+
+
+                    //  Get all files from Request object  
+                    System.Web.HttpFileCollectionBase files = Request.Files;
+                    var _ext = "";
+                    var fileName = "";
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        HttpPostedFileBase file = files[i];
+                        // Checking for Internet Explorer  
+                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER" || Request.Browser.Browser.ToUpper() == "GOOGLE CHROME")
+                        {
+                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                            fname = testfiles[testfiles.Length - 1];
+                        }
+                        else
+                        {
+
+                            fname = file.FileName;
+                            fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                            _ext = System.IO.Path.GetExtension(fname).ToUpper();
+                            if ((_ext != ".JPG" && _ext != ".PNG" && _ext != ".GIF" && _ext != ".PDF"))
+                            {
+                                return Json("Error occurred. Error details: Only Image Or Pdf", JsonRequestBehavior.AllowGet);
+                            }
+
+                        }
+
+                        // Get the complete folder path and store the file inside it.
+                        _imgname1 = fileName + "" + System.DateTime.Now.ToString("ddMMyyyyHHMMss") + "" + _ext;
+                        _imgname = System.IO.Path.Combine(Server.MapPath(strMappath), _imgname1);
+                        file.SaveAs(_imgname);
+                    }
+                    // Returns message that successfully uploaded  
+                    return Json(strMappath.ToString().Replace("~", "") + _imgname1, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception ex)
+                {
+                    return Json("Error occurred. Error details: " + ex.Message);
+                }
+            }
+            else
+            {
+                return Json(strMappath.ToString().Replace("~", "") + _imgname1, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+        public ActionResult SendEmail(int LeaveRequestId, int LoginUserid)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                DataSet ds1 = new DataSet();
+                SqlParameter[] prm =
+                {
+                   new SqlParameter("@LeaveRequestId",LeaveRequestId)
+                };
+                ds = Com.ExecuteDataSet("RDD_GetLeaverequestDetailformail", CommandType.StoredProcedure, prm);
+
+                SqlParameter[] prm1 =
+                {
+                   new SqlParameter("@LoginUserId",LoginUserid)
+                };
+                ds1 = Com.ExecuteDataSet("RDD_GetManagerDetails", CommandType.StoredProcedure, prm1);
+                var LeaveName= ds.Tables[0].Rows[0]["LeaveName"];
+                var Fromdate = ds.Tables[0].Rows[0]["FromDate"];
+                var Todate = ds.Tables[0].Rows[0]["ToDate"];
+                
+                var L1ManagerName= ds1.Tables[0].Rows[0]["EmployeeName"].ToString();
+                var Email1 = ds1.Tables[0].Rows[0]["Email"].ToString();
+                var Email2 = "";
+                if(ds1.Tables[1].Rows.Count>0)
+                {
+                    Email2 = ds1.Tables[1].Rows[0]["Email"].ToString();
+                } 
+                var EmployeeName= ds1.Tables[2].Rows[0]["EmployeeName"].ToString();
+                var Email3 = ds1.Tables[2].Rows[0]["Email"].ToString();
+                
+                var Tomail = Email1;
+                var cc = Email3 + "," + Email2;
+                string Subject = "Leave Approval Request";
+                
+                var Html = "Dear " + L1ManagerName + ",<br/><br/>";
+                if (String.Format("{0:ddd, MMM d, yyyy}", Fromdate) == String.Format("{0:ddd, MMM d, yyyy}", Todate))
+                {
+                    Html = Html + "" + EmployeeName + " has applied for " + LeaveName + " " + "leave on" + " " + String.Format("{0:ddd, MMM d, yyyy}", Fromdate) + " and it is pending for approval by you.<br/><br/>";
+                }
+                else
+                {
+                    Html = Html + "" + EmployeeName + " has applied for " + LeaveName + " " + "leave from" + " " + String.Format("{0:ddd, MMM d, yyyy}", Fromdate) + " " + "to" + " " + String.Format("{0:ddd, MMM d, yyyy}", Todate) + " and it is pending for approval by you.<br/><br/>";
+                }                
+                Html = Html + "Best Regards, <br/> Red Dot Distribution";
+                Tomail = "mainak@reddotdistribution.com";
+                cc = "pratim.d@reddotdistribution.com";
+                SendMail.Send(Tomail, cc, Subject, Html, true);
+                
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Index", "RDD_LeaveRequest");
+        }
+        [Route("GETHOLIDAY")]
+        public ActionResult GetHolidayCountryWise(int EmployeeId)
+        {
+            ContentResult retVal = null;
+            DataSet ds;
+            try
+            {
+                ds = rDD_LeaveRequest_TemplatesDb.GetHolidayCountryWise(EmployeeId);
+                if (ds.Tables.Count > 0)
+                {
+                    retVal = Content(JsonConvert.SerializeObject(ds), "application/json");
+                }
+                return retVal;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
     }
---
+++ RDDStaffPortal/RDDStaffPortal/Scripts/LMS/LeaveManage.js
@@ -0,0 +1,265 @@
+﻿var LeaveManage = {
+    initialize: function (e) {
+        debugger
+       
+
+        LeaveManage.Attachevent();
+    },
+    Attachevent: function () {
+       
+        $(document).on('apply.daterangepicker','#reportrange', function (ev, picker) {
+            debugger;
+            var currentleave;
+            $("#random").hide();
+            $("#Leavedtls").empty();
+            var d = new Date();
+            var StrtDate = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
+            var EndDate = d.getFullYear() + "/" + 12 + "/" + 31;
+            var fromdate = $('#reportrange').data('daterangepicker').startDate.format('YYYY-MM-DD');
+            var todate = $('#reportrange').data('daterangepicker').endDate.format('YYYY-MM-DD');
+
+            var startMin1 = new Date(moment(fromdate, 'YYYY-MM-DD').add(-1, 'days'));
+
+            var days = daysdifference(fromdate, todate);
+            var arr2 = getDateArray(fromdate, todate);
+            var startMin = moment(fromdate, 'YYYY-MM-DD');
+            startMin = new Date(startMin);
+            var startdayMin = ("0" + startMin.getDate()).slice(-2);
+            var startmonthMin = ("0" + (startMin.getMonth() + 1)).slice(-2);
+            var nowMax = moment(todate, 'YYYY-MM-DD');
+            nowMax = new Date(nowMax);
+            var dayMax = ("0" + nowMax.getDate()).slice(-2);
+            var monthMax = ("0" + (nowMax.getMonth() + 1)).slice(-2);
+            $('#reportrange span').html(startdayMin + "/" + startmonthMin + "/" + startMin.getFullYear() + ' - ' + dayMax + "/" + monthMax + "/" + nowMax.getFullYear());
+            $(this).val(picker.startDate.format('MM/DD/YYYY') + '-' + picker.endDate.format('MM/DD/YYYY'));
+            $("#divDates").empty();
+            var weeklyoff = $("#Weeklyoff").val();
+            var LeaveRule = $("#LeaveRule").val();
+            var weekday = new Array(7);
+            weekday[0] = "Sunday";
+            weekday[1] = "Monday";
+            weekday[2] = "Tuesday";
+            weekday[3] = "Wednesday";
+            weekday[4] = "Thursday";
+            weekday[5] = "Friday";
+            weekday[6] = "Saturday";
+            if (LeaveRule != "WorkingDays") {
+                if (weekday[startMin1.getDay()] == weeklyoff) {
+                    $('#reportrange').data('daterangepicker').startDate.add(-1, 'day')
+                    html = '<tr class="dttype"><td style="width:50%; float:left">';
+                    html += '<div id="divLvDates">' + getMonthDetails((startMin1.getMonth())) + '' + startMin1.getDate() + '</div>';
+                    html += '</td><td style="width:50%; float:right">';
+                    html += '<select name="daytime" class="custom-select" id="ddlDayType"> <option value="1">All Day </option> <option value="0.5">half day - morning</option> <option value="0.5">half day - afternoon </option> </select >';
+                    html += '</td></tr>';
+                    $("#divDates").append(html);
+                }
+            }
+
+            for (var i = 0; i < arr2.length; i++) {
+
+                currentleave = (arr2[i].getMonth()) * 1.75;
+                var date1 = arr2[i].getDate();
+                var curyear = d.getFullYear();
+                var months = getMonthDetails((arr2[i].getMonth()));
+                var html = '<tr class="dttype"><td style="width:50%; float:left">';
+                html += '<div id="divLvDates">' + months + '' + date1 + '</div>';
+                html += '</td><td style="width:50%; float:right">';
+                html += '<select name="daytime" class="custom-select" id="ddlDayType"> <option value="1">All Day </option> <option value="0.5">half day - morning</option> <option value="0.5">half day - afternoon </option> </select >';
+                html += '</td></tr>';
+
+                if (LeaveRule == "WorkingDays") {
+
+                    if (weekday[arr2[i].getDay()] != weeklyoff) {
+
+
+                        $("#divDates").append(html);
+                    }
+                }
+                else {
+
+
+                    $("#divDates").append(html);
+
+                }
+
+            }
+            var remainleave = (currentleave - days);
+            var Header = '<h5>' + days + ' days of annual leave</h5>';
+            Header += '<h5>' + remainleave + ' days remaining</h5>';
+            $("#Leavedtls").append(Header);
+
+        });
+        var totalday = 0;
+
+        $(document).on('change', '[id^=EmployeeId]', function () {
+
+            var EmployeeId = $("#EmployeeId option:selected").val();
+            $("#Leavedtls").empty();
+            $("#divDates").empty();
+            $("#random").show();
+            $.ajax({
+                type: "POST",
+                url: "/GetWeeklyOffDay",
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify({ EmployeeId: $(this).val() }),
+
+
+                success: function (data) {
+
+                    arr = data;
+                    $("#Weeklyoff").val(arr.data[0].WeeklyOff);
+                    $("#LeaveRule").val(arr.data[0].LeaveRules);
+                }
+
+            });
+        });
+        var getDateArray = function (fromdate, todate) {
+
+
+            var arr = new Array();
+            var weekday = new Array(7);
+            weekday[0] = "Sunday";
+            weekday[1] = "Monday";
+            weekday[2] = "Tuesday";
+            weekday[3] = "Wednesday";
+            weekday[4] = "Thursday";
+            weekday[5] = "Friday";
+            weekday[6] = "Saturday";
+            var arr3 = new Array();
+            var dt = new Date(fromdate);
+            var fnldt = new Date(todate);
+            while (dt <= fnldt) {
+
+                arr.push(new Date(dt));
+                arr3.push(weekday[dt.getDay()]);
+                dt.setDate(dt.getDate() + 1);
+            }
+
+            return arr;
+
+        }
+
+        function daysdifference(FromDate, ToDate) {
+
+            var startDay = new Date(FromDate);
+            var endDay = new Date(ToDate);
+            var millisBetween = endDay.getTime() - startDay.getTime();
+            var days = millisBetween / (1000 * 3600 * 24);
+            var finalday = days + 1;
+            return finalday;
+        }
+        function getMonthDetails(month) {
+            var monthnm;
+            if (month == 0) {
+                monthnm = "Jan";
+            }
+            else if (month == 1) {
+                monthnm = "Feb";
+            }
+            else if (month == 2) {
+                monthnm = "Mar";
+            }
+            else if (month == 3) {
+                monthnm = "Apr";
+            }
+            else if (month == 4) {
+                monthnm = "May";
+            }
+            else if (month == 5) {
+                monthnm = "Jun";
+            }
+            else if (month == 6) {
+                monthnm = "July";
+            }
+            else if (month == 7) {
+                monthnm = "Aug";
+            }
+            else if (month == 8) {
+                monthnm = "Sep";
+            }
+            else if (month == 9) {
+                monthnm = "Oct";
+            }
+            else if (month == 10) {
+                monthnm = "Nov";
+            }
+            else if (month == 11) {
+                monthnm = "Dec";
+            }
+            else {
+                monthnm = "undefined";
+            }
+            return monthnm;
+
+        }
+        
+        $(document).on('click', '#Btn_Submit', function () {
+            $(".loader1").show();
+            if ($('#Reason').val().length == 0) {
+                RedDotAlert_Error("Please Enter a valid reason");
+                $(".loader1").hide();
+                return false;
+            }
+            $(".dttype").each(function () {
+                debugger;
+                var currow = $(this).closest('tr');
+                var LeaveDate = $(this).find("[id^='divLvDates']").html();
+                var LeaveDay = $(this).find("[id^='ddlDayType']").val();
+                var LeaveDayType = $(this).find("[id^='ddlDayType'] option:selected").text();
+                totalday += parseFloat(LeaveDay);
+                var LeaveRequestDetails = {
+                    LeaveDate: LeaveDate,
+                    LeaveDay: LeaveDay,
+                    LeaveDayType: LeaveDayType
+                };
+            });
+            var fromdate = $('#reportrange').data('daterangepicker').startDate.format('YYYY-MM-DD');
+            var todate = $('#reportrange').data('daterangepicker').endDate.format('YYYY-MM-DD');
+            var days = daysdifference(fromdate, todate);
+            var day = totalday;
+            var RDD_LeaveRequest = {
+                EmployeeId: $("#EmployeeId option:selected").val(),
+                LeaveTypeId: $("#LeaveTypeId option:selected").val(),
+                Reason: $("#Reason").val(),
+                FromDate: fromdate,
+                ToDate: todate,
+                NoOfDays: day,
+                LeaveStatus: "Pending",
+                ApproverRemarks: "NULL",
+                IsPrivateLeave: $('input[name="private"]:checked').val(),
+                AttachmentUrl: $("#AttachmentUrl[type='hidden']").val(),
+                LeaveRequestDetailsList: []
+            };
+
+            $(".dttype").each(function () {
+                var currow = $(this).closest('tr');
+                var LeaveDate = $(this).find("[id^='divLvDates']").html();
+                var LeaveDay = $(this).find("[id^='ddlDayType']").val();
+                var LeaveDayType = $(this).find("[id^='ddlDayType'] option:selected").text();
+                var LeaveRequestDetails = {
+                    LeaveDate: LeaveDate,
+                    LeaveDay: LeaveDay,
+                    LeaveDayType: LeaveDayType
+                };
+                RDD_LeaveRequest.LeaveRequestDetailsList.push(LeaveRequestDetails);
+            });
+
+            $.post("/SaveLeaveRequest", { RDD_LeaveRequest: RDD_LeaveRequest }, function (response) {
+                debugger
+                $(".loader1").hide();
+                if (response.data.Saveflag == true && response.data.LeaveRequestId != -1) {
+                    $("#exampleModal").modal("hide");
+                    RedDotAlert_Success(response.data.ErrorMsg);
+                    window.location.href = '@Url.Action("Index","RDD_LeaveRequest")';
+
+                }
+                else {
+                    RedDotAlert_Error(response.data.ErrorMsg);
+                }
+
+            });
+
+        });
+    }
+}
